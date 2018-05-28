259ee34786926b71f188be492ec0276af82b9ac5
Chit Piotr <175015@centrala.bzwbk>
2018-05-28 12:11:50 +0200

    Changing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,45 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { discardPeriodicTasks } from '@angular/core/testing';
 import { Injectable, Pipe, PipeTransform } from '@angular/core';
-import{SearchFilterPipe} from './filter.pipe'
+import { SearchFilterPipe } from './filter.pipe';
+import { Task } from './task/task.component'
+
 @Component({
   selector: 'app-root',
-
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-
 })
-export class AppComponent implements OnInit{
+
+export class AppComponent implements OnInit {
   tasks: Task[] = [];
-  newTask = "  ";
+  newTask: string = '';
   id = 1;
   itemFilter: any = { text: '' };
   tasksDone = [];
-   title:String;
-constructor() { 
-  this.title = " ";
-  this.tasks = []
-}
+  title: string;
 
-ngOnInit() {
-}
+  ngOnInit() {
+    this.title = '';
+    this.tasks = []
+  }
 
-
-
-  pushTask = function () {
-    // jesli ten element jest pusty dodaje nowy task
+  pushTask = function (index) {
     if (this.newTask) {
       const newTaskObject: Task = {
         title: this.newTask,
         date: new Date(),
         done: false
       };
+
       this.tasks.push(newTaskObject);
       this.newTask = " ";
     }
   }
 
-  // usuwanie
   removeTask = function (index) {
     this.tasks.splice(index, 1);
   }
-  removeTask1 = function (index) {
+  removeChecked = function (index) {
     this.tasksDone.splice(index, 1);
 
   }
@@ -56,11 +52,11 @@ ngOnInit() {
     this.tasksDone.splice(ind, 1);
     this.tasksDone.push(task);
   }
-  sortNowe() {
+  sortNewest() {
     this.tasksDone = this.tasksDone.sort((a, b) => a.date.getTime() - b.date.getTime());
     this.tasks = this.tasks.sort((a, b) => a.date.getTime() - b.date.getTime());
   }
-  sortStare() {
+  sortOldest() {
     this.tasksDone = this.tasksDone.sort((a, b) => b.date.getTime() - a.date.getTime());
     this.tasks = this.tasks.sort((a, b) => b.date.getTime() - a.date.getTime());
   }
@@ -71,11 +67,11 @@ ngOnInit() {
   sortZA() {
     this.tasks = this.tasks.sort((b, a) => a.title.localeCompare(b.title));
     this.tasksDone = this.tasksDone.sort((a, b) => b.title.localeCompare(a.title));
- }
+  }
 
 }
-export interface Task {
-  title: string;
-  date: Date;
-  done: boolean;
-}
+// export interface Task {
+//   title: string;
+//   date: Date;
+//   done: boolean;
+// }
--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,21 +1,21 @@
 
-  import {Pipe, PipeTransform, Injectable,  } from '@angular/core';
+import { Pipe, PipeTransform, Injectable, } from '@angular/core';
 
-  @Pipe({
-    name: 'filter'
-  })
-  
-  export class SearchFilterPipe implements PipeTransform {
+@Pipe({
+  name: 'filter'
+})
 
-    transform(tasks: any[], searchText: string): any[] {
-   
-  
+export class SearchFilterPipe implements PipeTransform {
+
+  transform(tasks: any[], searchText: string): any[] {
+
+
     if (searchText) {
       searchText = searchText.toLowerCase();
       return tasks.filter(function (el: any) {
-          return el.title.toLowerCase().indexOf(searchText) > -1;
+        return el.title.toLowerCase().indexOf(searchText) > -1;
       })
+    }
+    return tasks;
   }
-  return tasks;
-}
 }
