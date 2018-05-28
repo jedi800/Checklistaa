90f5a80b7da808b98b7520668833272aa258d6d7
Chit Piotr <175015@centrala.bzwbk>
2018-05-28 15:31:59 +0200

    importy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { discardPeriodicTasks } from '@angular/core/testing';
 import { Injectable, Pipe, PipeTransform } from '@angular/core';
-import { Task } from './task/task.component';
 import { SearchFilterPipe } from './filter.pipe';
+import { Task } from './task/task.component';
 
 
 @Component({
