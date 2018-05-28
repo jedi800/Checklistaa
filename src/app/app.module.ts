3d5cb58aee41d9b2ccc9d4eeaf5f84dbbd2a919d
Chit Piotr <175015@centrala.bzwbk>
2018-05-28 14:05:10 +0200

    importy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,25 @@ import { NgModule } from '@angular/core';
 import { Injectable, Pipe, PipeTransform } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { SearchFilterPipe } from './filter.pipe';  
-// import { SearchPipe } from "./filter.pipe";
-import { Ng2FilterPipeModule } from 'ng2-filter-pipe';  
+import { SearchFilterPipe } from './filter.pipe';
+import { Task } from './task/task.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     SearchFilterPipe,
-    // SearchPipe
   ],
+
   imports: [
     BrowserModule,
     FormsModule,
-    Ng2FilterPipeModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
- 
+
+
--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,21 +1,19 @@
+import { Pipe, PipeTransform, Injectable, } from '@angular/core';
 
-  import {Pipe, PipeTransform, Injectable,  } from '@angular/core';
+@Pipe({
+  name: 'filter'
+})
 
-  @Pipe({
-    name: 'filter'
-  })
-  
-  export class SearchFilterPipe implements PipeTransform {
+export class SearchFilterPipe implements PipeTransform {
 
-    transform(tasks: any[], searchText: string): any[] {
-   
-  
+  transform(tasks: any[], searchText: string): any[] {
+
     if (searchText) {
       searchText = searchText.toLowerCase();
       return tasks.filter(function (el: any) {
-          return el.title.toLowerCase().indexOf(searchText) > -1;
+        return el.title.toLowerCase().indexOf(searchText) > -1;
       })
+    }
+    return tasks;
   }
-  return tasks;
-}
 }
