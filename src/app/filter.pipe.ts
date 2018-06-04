23283ee81b343ef5a1c6dc9585aa6cc4598d935e
Chit Piotr <175015@centrala.bzwbk>
2018-06-04 13:31:17 +0200

    Strzałkowa

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -5,14 +5,10 @@ import { Pipe, PipeTransform, Injectable, } from '@angular/core';
 })
 
 export class SearchFilterPipe implements PipeTransform {
-
   transform(tasks: any[], searchText: string): any[] {
-
     if (searchText) {
       searchText = searchText.toLowerCase();
-      return tasks.filter(function (el: any) {
-        return el.title.toLowerCase().indexOf(searchText) > -1;
-      })
+      return tasks.filter(el => el.title.toLowerCase().indexOf(searchText) > -1);
     }
     return tasks;
   }
